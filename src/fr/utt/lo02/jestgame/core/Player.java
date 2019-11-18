e0427f379b22debcc1778ef1f4e3e4eca1a1644e
mr-pencil <akram.syukri@icloud.com>
2019-11-18 19:18:43 +0100

    ajoute des variables et aussi un constructeur sur la classe Player

--- src/fr/utt/lo02/jestgame/core/Player.java
+++ src/fr/utt/lo02/jestgame/core/Player.java
@@ -6,5 +6,25 @@ import java.util.List;
 import fr.utt.lo02.jestgame.api.ICard;
 
 public class Player {
+	
 	private List<ICard> hand = new ArrayList<ICard>();
+	private List<ICard> capturedCards = new ArrayList<ICard>();
+	private String name;
+	private int score;
+	private int rank;
+	private boolean isFacedUp;
+	private boolean isCatchedUp;
+	private int facedUpRank;
+	private IObserver interfac;
+	
+	
+	public Player(String name, int rank, IObserver interfac) {
+		this.name=name;
+		this.rank=rank;
+		this.interfac=interfac;
+		
+	}
+	
+	
+	
 }
