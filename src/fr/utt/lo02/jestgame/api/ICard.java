42b3b85e836f7867e9219d15a2c6e88866f20565
mr-pencil <akram.syukri@icloud.com>
2019-11-20 20:37:08 +0100

    ajoute methode dans interface ICard

--- src/fr/utt/lo02/jestgame/api/ICard.java
+++ src/fr/utt/lo02/jestgame/api/ICard.java
@@ -1,5 +1,15 @@
 package fr.utt.lo02.jestgame.api;
 
+import fr.utt.lo02.jestgame.core.Player;
+
 public interface ICard {
+	
+	public Player chooseTrophyOwner(Player[] players);
+	public int getUpdatedGameFaceValue(Player[] players);
+	public int endFaceValue(Player[] players);
+	public int endSpecialFaceValue(Player[] players);
+	public String getName();
+	public String getColor();
+	public int getColorValue();
 
 }
