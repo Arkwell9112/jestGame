5dad1bb18e011a631e4914562b3438334903dac9
mr-pencil <akram.syukri@icloud.com>
2019-11-21 18:04:22 +0100

    Creation d'une classe HumanPlayer et BotPlayer que avec ses methodes(non complet)

---
+++ src/fr/utt/lo02/jestgame/core/BotPlayer.java
@@ -0,0 +1,27 @@
+package fr.utt.lo02.jestgame.core;
+
+public class BotPlayer {
+	
+	public void yourTurnFaceUp() {
+		
+	}
+	
+	public void yourTurnCatchUp(Player[] players) {
+		
+		
+	}
+	
+	protected void  chooseFaceUp(Player[] players) {
+		
+	}
+	
+	protected void chooseCatchUp(Player[] players) {
+		
+	}
+	
+	public void notifyBack(NotEvent event, Object[] args) {
+		
+	}
+	
+
+}
---
+++ src/fr/utt/lo02/jestgame/core/HumanPlayer.java
@@ -0,0 +1,17 @@
+package fr.utt.lo02.jestgame.core;
+
+public class HumanPlayer {
+	
+	public void yourTurnFaceUp() {
+		
+	}
+	
+	public void yourTurnCatchUp(String[] players) {
+		
+	}
+	
+	public void notifyBack(NotEvent event, Object[] args) {
+		
+	}
+
+}
