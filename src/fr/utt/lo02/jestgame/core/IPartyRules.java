989f9ce3733b624076f5e30190a5965038f473fb
mr-pencil <akram.syukri@icloud.com>
2019-11-24 11:42:42 +0100

    mettre les methodes dans interface IPartyRules

--- src/fr/utt/lo02/jestgame/core/IPartyRules.java
+++ src/fr/utt/lo02/jestgame/core/IPartyRules.java
@@ -2,6 +2,11 @@ package fr.utt.lo02.jestgame.core;
 
 public interface IPartyRules {
 	
+	public Player chooseFirstCatch(Player[] players);
+	public Player chooseWinner(Player[] players);
+	public int getPlayerCardNb();
+	public int getTrophyCardNb();
+	
 	
 		
 		
