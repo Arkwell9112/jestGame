7ee1a9a02342d1563240353f13577fdbe8e38e5c
Arkwell9112 <edouard.berge@utt.fr>
2019-11-25 11:48:33 +0100

    Déplacer IPartyRules vers le package api

--- src/fr/utt/lo02/jestgame/api/ICard.java
+++ src/fr/utt/lo02/jestgame/api/ICard.java
@@ -1,10 +1,12 @@
 package fr.utt.lo02.jestgame.api;
 
+import java.util.List;
+
 import fr.utt.lo02.jestgame.core.Player;
 
 public interface ICard {
 	
-	public Player chooseTrophyOwner(Player[] players);
+	public Player chooseTrophyOwner(List<Player> players);
 	public int getUpdatedGameFaceValue(Player[] players);
 	public int endFaceValue(Player[] players);
 	public int endSpecialFaceValue(Player[] players);
---
+++ src/fr/utt/lo02/jestgame/api/IPartyRules.java
@@ -0,0 +1,19 @@
+package fr.utt.lo02.jestgame.api;
+
+import java.util.List;
+
+import fr.utt.lo02.jestgame.core.Player;
+
+public interface IPartyRules {
+	
+	public Player chooseFirstCatch(List<Player> players);
+	public Player chooseWinner(Player[] players);
+	public int getPlayerCardNb();
+	public int getTrophyCardNb();
+	
+	
+		
+		
+	
+
+}
